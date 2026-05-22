1d89b05f2c98ffc6ce7f5cde694ff427a2bd8bc4
yppeny <iehnugqmr6176@hotmail.com>
2026-05-22 09:37:51 +0800

    Deploy sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'family-menu-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
